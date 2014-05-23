a9927d5ee14086623c76229e494ba07d36aa6dcc
gabrielnaves <gabri.navess@gmail.com>
2014-05-23 10:29:28 -0300

    Corrected the BuildMovementMap method on MovementMap class.

--- IDJ/src/engine/cpp/MovementMap.cpp
+++ IDJ/src/engine/cpp/MovementMap.cpp
@@ -36,21 +36,29 @@ void MovementMap::BuildMovementMap(std::string file, int layer)
     getline(mapFile, str);
     getline(mapFile, str);
 
+    if (layer >= mapDepth || layer < 0)
+        { std::cerr << "ERROR: Invalid layer!" << std::endl; return; }
+
     int i = 0, j = 0, k = 0;
     std::string num;
     num.resize(2);
     movementMatrix.resize(mapWidth*mapHeight);
-    for (unsigned int i = 0; i < movementMatrix.size(); i++) movementMatrix[i] = -1;
+    for (int i = 0; i < (int)movementMatrix.size(); i++) movementMatrix[i] = -1;
 
-    while (getline(mapFile, str))
+    while (k < layer)
     {
-        if (str.size() == 0)
+        for (j = 0; j < mapHeight; j++)
         {
-            if (k == layer) break;
-            ++k;
-            i = j = 0;
             getline(mapFile, str);
         }
+        getline(mapFile, str);
+        k++;
+    }
+
+    j = 0;
+    while (getline(mapFile, str))
+    {
+        if (str.size() == 0) break;
         while (str.size() > 1)
         {
             num[0] = str[0], num[1] = str[1];
