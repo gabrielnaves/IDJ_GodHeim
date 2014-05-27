cc6c1a80b48d315fa175d951ab4aa0ceadda3359
gabrielnaves <gabri.navess@gmail.com>
2014-05-27 12:51:09 -0300

    Corrected some names.

--- IDJ/src/specificCode/gameStates/cpp/Level1State.cpp
+++ IDJ/src/specificCode/gameStates/cpp/Level1State.cpp
@@ -8,15 +8,15 @@
 
 #include "../include/Level1State.h"
 
-Level1State::Level1State() : State(), tileSet(55,55,"img/tiles2.png"),
-                             tileMap("map/firstLevel/firstLayer.txt", &tileSet)
+Level1State::Level1State() : State(), tileSet(55,55,"img/firstLevelTiles.png"),
+                             tileMap("map/firstLevel.txt", &tileSet)
 {
     tempBlackBackground.Open("img/Canvas Master.png");
     bg.Open("img/background primeira fase tamanho total.png");
     rochas.Open("img/rochas.png");
 
-    objectArray.emplace_back(new Loki(70,100, MovementMap("map/firstLevel/firstLayer.txt", tileSet)));
-    objectArray.emplace_back(new Thor(20,100, MovementMap("map/firstLevel/firstLayer.txt", tileSet)));
+    objectArray.emplace_back(new Loki(70,100, MovementMap("map/firstLevel.txt", tileSet)));
+    objectArray.emplace_back(new Thor(20,100, MovementMap("map/firstLevel.txt", tileSet)));
     Barrier *barrier = new Barrier();
     Camera::Follow(barrier);
     objectArray.emplace_back(barrier);
