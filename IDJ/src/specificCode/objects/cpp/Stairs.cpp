b0c5ca8bb908f74634ab2b72ee55736b4bcbdd01
gabrielnaves <gabri.navess@gmail.com>
2014-06-08 12:35:23 -0300

    Added the Stairs object.

---
+++ IDJ/src/specificCode/objects/cpp/Stairs.cpp
@@ -0,0 +1,20 @@
+/*
+ * Stairs.cpp
+ *
+ *  Created on: Jun 8, 2014
+ *      Author: Gabriel Naves
+ */
+
+#include "../include/Stairs.h"
+
+Stairs::Stairs()
+{
+    // TODO Auto-generated constructor stub
+
+}
+
+Stairs::~Stairs()
+{
+    // TODO Auto-generated destructor stub
+}
+
---
+++ IDJ/src/specificCode/objects/include/Stairs.h
@@ -0,0 +1,20 @@
+/*
+ * Stairs.h
+ *
+ *  Created on: Jun 8, 2014
+ *      Author: Gabriel Naves
+ */
+
+#ifndef STAIRS_H_
+#define STAIRS_H_
+
+#include "../../../engine/include/GameObject.h"
+
+class Stairs: public GameObject
+{
+public:
+    Stairs();
+    virtual ~Stairs();
+};
+
+#endif /* STAIRS_H_ */
