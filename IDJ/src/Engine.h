94454618f6a098344262a3c4588c0559fd125022
gabrielnaves <gabri.navess@gmail.com>
2014-06-15 19:06:40 -0300

    Created Engine.h file, wich includes every header file on engine folder. Level1State.h now includes Engine.h.

---
+++ IDJ/src/Engine.h
@@ -0,0 +1,34 @@
+/*
+ * Engine.h
+ *
+ *  Created on: Jun 15, 2014
+ *      Author: Gabriel Naves
+ */
+
+#ifndef ENGINE_H_
+#define ENGINE_H_
+
+#include "engine/include/BlockTileSet.h"
+#include "engine/include/Camera.h"
+#include "engine/include/Collision.h"
+#include "engine/include/Game.h"
+#include "engine/include/GameObject.h"
+#include "engine/include/InputManager.h"
+#include "engine/include/LineSegment.h"
+#include "engine/include/MovementMap.h"
+#include "engine/include/MultiTileSet.h"
+#include "engine/include/Music.h"
+#include "engine/include/Point.h"
+#include "engine/include/Rect.h"
+#include "engine/include/Resource.h"
+#include "engine/include/Sound.h"
+#include "engine/include/Sprite.h"
+#include "engine/include/State.h"
+#include "engine/include/StateData.h"
+#include "engine/include/StillAnimation.h"
+#include "engine/include/Text.h"
+#include "engine/include/TileMap.h"
+#include "engine/include/TileSet.h"
+#include "engine/include/Timer.h"
+
+#endif /* ENGINE_H_ */
--- IDJ/src/specificCode/gameStates/include/Level1State.h
+++ IDJ/src/specificCode/gameStates/include/Level1State.h
@@ -9,15 +9,7 @@
 #ifndef LEVEL1STATE_H_
 #define LEVEL1STATE_H_
 
-#include "../../../engine/include/Collision.h"
-#include "../../../engine/include/Camera.h"
-#include "../../../engine/include/State.h"
-#include "../../../engine/include/Sprite.h"
-#include "../../../engine/include/Music.h"
-#include "../../../engine/include/Sound.h"
-#include "../../../engine/include/BlockTileSet.h"
-#include "../../../engine/include/TileMap.h"
-#include "../../../engine/include/MovementMap.h"
+#include "../../../Engine.h"
 #include "../../characters/include/Barrier.h"
 #include "../../objects/include/BrokenHouse.h"
 #include "../../objects/include/Stairs.h"
