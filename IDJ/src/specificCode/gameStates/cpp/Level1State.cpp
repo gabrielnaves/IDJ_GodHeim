1574931e1458ef880db1ec5ca8161169bb14f1b8
imota <mota.icaro@gmail.com>
2014-07-09 01:10:32 -0300

    Added more plants

--- IDJ/src/specificCode/gameStates/cpp/Level1State.cpp
+++ IDJ/src/specificCode/gameStates/cpp/Level1State.cpp
@@ -101,6 +101,8 @@ void Level1State::EmplaceInitialObjects()
     objectArray.emplace_back(new Plants("img/level1/plants/9.png",640,375));
     objectArray.emplace_back(new Plants("img/level1/plants/10.png",610,375));
     objectArray.emplace_back(new PlantBarrier("img/objects/plant.png",870,880));
+    objectArray.emplace_back(new PlantBarrier("img/objects/plant.png",550,880));
+    objectArray.emplace_back(new PlantBarrier("img/objects/plant.png",220,880));
     objectArray.emplace_back(new Rock("img/objects/pedra.png",1045,1045));
     objectArray.emplace_back(new StoneBlock("img/objects/pedradupla.png",11*55,1540));
     objectArray.emplace_back(new Goat(800, 310));
